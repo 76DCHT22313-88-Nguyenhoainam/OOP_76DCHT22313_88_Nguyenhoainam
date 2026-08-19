e7589865a5c58b8df7af1bc2d74cb9039514c90d
banhdauxanhh <nghna.src@gmail.com>
2026-08-19 21:20:41 +0700

    BT01-CP1: tao cau truc nhaanvien, khai bao mang, so luong nv

---
+++ BT01_On_tap_C++_co_so/BT01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct NhanVien {
+    string maNV;
+    string tenNV;
+    float luongCoBan;
+    float heSo;
+};
+
+int main() {
+    int n;
+    cout << "Nhap so luong nhan vien: ";
+    cin >> n;
+
+    NhanVien dsNV[100]; 
+    
+    return 0;
+}
