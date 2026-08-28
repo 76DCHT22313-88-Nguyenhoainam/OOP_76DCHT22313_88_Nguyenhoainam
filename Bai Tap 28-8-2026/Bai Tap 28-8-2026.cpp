8bbc5245f996504c7a0de79f8dc409dcf2ac3617
banhdauxanhh <nghna.src@gmail.com>
2026-08-28 10:14:16 +0700

    CP2: Xay dung ham nhap xuat

--- Bai Tap 28-8-2026/Bai Tap 28-8-2026.cpp
+++ Bai Tap 28-8-2026/Bai Tap 28-8-2026.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 using namespace std;
 
-// Cau 1: Khai bao lop ma tran
 class MaTran {
 private:
     int soHang;
     int soCot;
-    int pt[50][50]; // Mang 2 chieu chua cac phan tu cua ma tran
+    int pt[50][50];
+
+public:
+    // Cau 2: Phuong thuc nhap
+    void nhap() {
+        cout << "Nhap so hang: "; cin >> soHang;
+        cout << "Nhap so cot: "; cin >> soCot;
+        for(int i = 0; i < soHang; i++) {
+            for(int j = 0; j < soCot; j++) {
+                cout << "Phan tu [" << i << "][" << j << "] = ";
+                cin >> pt[i][j];
+            }
+        }
+    }
+
+    // Cau 2: Phuong thuc xuat
+    void xuat() {
+        for(int i = 0; i < soHang; i++) {
+            for(int j = 0; j < soCot; j++) {
+                cout << pt[i][j] << "\t";
+            }
+            cout << endl;
+        }
+    }
 };
 
 int main() {
+    
+    MaTran mt;
+    cout << "--- NHAP MA TRAN ---" << endl;
+    mt.nhap();
+    cout << "--- MA TRAN VUA NHAP ---" << endl;
+    mt.xuat();
     return 0;
 }
