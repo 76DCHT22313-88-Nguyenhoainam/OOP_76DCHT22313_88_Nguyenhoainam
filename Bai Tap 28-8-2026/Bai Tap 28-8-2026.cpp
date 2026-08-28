30060c500becf148207191483e5fbdaec5e1989b
banhdauxanhh <nghna.src@gmail.com>
2026-08-28 10:10:51 +0700

    CP1: Khai bao lop MaTran

---
+++ Bai Tap 28-8-2026/Bai Tap 28-8-2026.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Cau 1: Khai bao lop ma tran
+class MaTran {
+private:
+    int soHang;
+    int soCot;
+    int pt[50][50]; // Mang 2 chieu chua cac phan tu cua ma tran
+};
+
+int main() {
+    return 0;
+}
