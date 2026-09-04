b1851820a022897b09be2018a374e94b060e8829
banhdauxanhh <nghna.src@gmail.com>
2026-09-04 19:56:17 +0700

    CP2: Xay dung nhap, xuat, tinh DTB

--- Bai Tap 04-09-2026/Bai 7.cpp
+++ Bai Tap 04-09-2026/Bai 7.cpp
@@ -2,15 +2,49 @@
 #include <string>
 using namespace std;
 
-// Cau 1: Khai bao lop SinhVien voi 4 mon hoc
 class SinhVien {
 private:
     string hoTen;
     int namSinh;
-    float diem[4]; // Mang chua diem 4 mon hoc, khong phai 5
+    float diem[4];
+
+public:
+    // Cau 2: Phuong thuc nhap
+    void nhap() {
+        cin.ignore(); 
+        cout << "Nhap ho ten: ";
+        getline(cin, hoTen);
+        cout << "Nhap nam sinh: ";
+        cin >> namSinh;
+        
+        cout << "Nhap diem 4 mon hoc:" << endl;
+        // Vong lap chi chay 4 lan
+        for(int i = 0; i < 4; i++) {
+            cout << " - Diem mon " << i + 1 << ": ";
+            cin >> diem[i];
+        }
+    }
+
+    // Cau 2: Phuong thuc tinh diem trung binh
+    float tinhDTB() {
+        float tong = 0;
+        for(int i = 0; i < 4; i++) {
+            tong = tong + diem[i];
+        }
+        return tong / 4.0; // Chia 4 vi chi co 4 mon
+    }
+
+    // Cau 2: Phuong thuc xuat
+    void xuat() {
+        cout << "- " << hoTen << " | Nam sinh: " << namSinh << " | Diem TB: " << tinhDTB() << endl;
+    }
 };
 
 int main() {
-    // Khung chuong trinh
+    SinhVien sv;
+    cout << "--- TEST NHAP 1 SINH VIEN ---" << endl;
+    sv.nhap();
+    cout << "\n--- THONG TIN VUA NHAP ---" << endl;
+    sv.xuat();
     return 0;
 }
