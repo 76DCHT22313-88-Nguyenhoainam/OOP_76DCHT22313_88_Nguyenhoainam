eba508f3e5fae2c9c38676fdca1210f6f2d8e881
banhdauxanhh <nghna.src@gmail.com>
2026-09-04 19:55:44 +0700

    CP1: Khai bao lop SinhVien

---
+++ Bai Tap 04-09-2026/Bai 7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Cau 1: Khai bao lop SinhVien voi 4 mon hoc
+class SinhVien {
+private:
+    string hoTen;
+    int namSinh;
+    float diem[4]; // Mang chua diem 4 mon hoc, khong phai 5
+};
+
+int main() {
+    // Khung chuong trinh
+    return 0;
+}
