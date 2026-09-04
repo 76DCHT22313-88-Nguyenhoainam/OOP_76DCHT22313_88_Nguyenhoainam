2d5cc472f22ee8660ff072de002f1370eea0e9ea
banhdauxanhh <nghna.src@gmail.com>
2026-09-04 19:52:31 +0700

    CP2: Xay dung phuong thuc nhap, xuat, tinh DTB

--- Bai Tap 04-09-2026/Bai 6.cpp
+++ Bai Tap 04-09-2026/Bai 6.cpp
@@ -2,15 +2,48 @@
 #include <string>
 using namespace std;
 
-// Cau 1: Khai bao lop SinhVien voi cac thuoc tinh co ban
 class SinhVien {
 private:
     string hoTen;
     int namSinh;
-    float diem[5]; // Mang chua diem 5 mon hoc
+    float diem[5];
+
+public:
+    // Cau 2: Phuong thuc nhap
+    void nhap() {
+        cin.ignore(); 
+        cout << "Nhap ho ten: ";
+        getline(cin, hoTen);
+        cout << "Nhap nam sinh: ";
+        cin >> namSinh;
+        
+        cout << "Nhap diem 5 mon hoc:" << endl;
+        for(int i = 0; i < 5; i++) {
+            cout << " - Diem mon " << i + 1 << ": ";
+            cin >> diem[i];
+        }
+    }
+
+    // Cau 2: Phuong thuc tinh diem trung binh
+    float tinhDTB() {
+        float tong = 0;
+        for(int i = 0; i < 5; i++) {
+            tong = tong + diem[i];
+        }
+        return tong / 5.0; 
+    }
+
+    // Cau 2: Phuong thuc xuat
+    void xuat() {
+        cout << hoTen << " | Nam sinh: " << namSinh << " | Diem TB: " << tinhDTB() << endl;
+    }
 };
 
 int main() {
-    // Khung chuong trinh
+    SinhVien sv;
+    cout << "--- TEST NHAP 1 SINH VIEN ---" << endl;
+    sv.nhap();
+    cout << "\n--- THONG TIN VUA NHAP ---" << endl;
+    sv.xuat();
     return 0;
 }
