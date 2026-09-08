6adad9321d6f34cb9c6f632d05afd5530bd08eca
banhdauxanhh <nghna.src@gmail.com>
2026-09-08 21:07:43 +0700

    CP2: Nap chong toan tu nhap xuat va rut gon bai 2

--- Bai Tap 07-09-2026/Bai2.cpp
+++ Bai Tap 07-09-2026/Bai2.cpp
@@ -2,17 +2,48 @@
 #include <cmath>
 using namespace std;
 
-// Cau 1: Khai bao lop PhanSo
 class PhanSo {
 private:
     int tuSo;
     int mauSo;
 
 public:
-    // Cau 1: Ham tao va ham huy
     PhanSo() { tuSo = 0; mauSo = 1; }
     PhanSo(int t, int m) { tuSo = t; mauSo = m; }
     ~PhanSo() {}
+
+    int timUCLN(int a, int b) {
+        a = abs(a); b = abs(b);
+        while (a * b != 0) {
+            if (a > b) a = a % b;
+            else b = b % a;
+        }
+        return a + b;
+    }
+
+    // Cau 2: Phuong thuc rut gon
+    void rutGon() {
+        int ucln = timUCLN(tuSo, mauSo);
+        tuSo = tuSo / ucln;
+        mauSo = mauSo / ucln;
+        if (mauSo < 0) { tuSo = -tuSo; mauSo = -mauSo; }
+    }
+
+    // Cau 2: Nap chong toan tu Nhap (>>) va Xuat (<<)
+    friend istream& operator>>(istream& is, PhanSo& ps) {
+        cout << "Nhap tu so: "; is >> ps.tuSo;
+        do {
+            cout << "Nhap mau so (khac 0): "; is >> ps.mauSo;
+        } while (ps.mauSo == 0);
+        return is;
+    }
+
+    friend ostream& operator<<(ostream& os, PhanSo ps) {
+        if (ps.tuSo == 0) os << 0;
+        else if (ps.mauSo == 1) os << ps.tuSo;
+        else os << ps.tuSo << "/" << ps.mauSo;
+        return os;
+    }
 };
 
 int main() {
