dc5e8a10d8324555aff254f18b3bd15770bc8b02
banhdauxanhh <nghna.src@gmail.com>
2026-09-08 21:06:09 +0700

    CP2: Xay dung phuong thuc nhap xuat va tinh toan bai 1

--- Bai Tap 07-09-2026/Bai1.cpp
+++ Bai Tap 07-09-2026/Bai1.cpp
@@ -2,27 +2,78 @@
 #include <cmath> 
 using namespace std;
 
-// Cau 1: Khai bao lop PhanSo
 class PhanSo {
 private:
     int tuSo;
     int mauSo;
 
 public:
-    // Cau 1: Ham tao khong doi (Mac dinh la 0/1)
-    PhanSo() {
-        tuSo = 0;
-        mauSo = 1;
+    PhanSo() { tuSo = 0; mauSo = 1; }
+    PhanSo(int t, int m) { tuSo = t; mauSo = m; }
+    ~PhanSo() {}
+
+    // Ham phu tro: Tim UCLN de rut gon
+    int timUCLN(int a, int b) {
+        a = abs(a); b = abs(b);
+        while (a * b != 0) {
+            if (a > b) a = a % b;
+            else b = b % a;
+        }
+        return a + b;
     }
 
-    // Cau 1: Ham tao co doi
-    PhanSo(int t, int m) {
-        tuSo = t;
-        mauSo = m;
+    // Cau 2: Phuong thuc nhap, xuat, rut gon
+    void nhap() {
+        cout << "Nhap tu so: "; cin >> tuSo;
+        do {
+            cout << "Nhap mau so (khac 0): "; cin >> mauSo;
+        } while (mauSo == 0);
     }
 
-    // Cau 1: Ham huy
-    ~PhanSo() {
+    void xuat() {
+        if (tuSo == 0) cout << 0;
+        else if (mauSo == 1) cout << tuSo;
+        else cout << tuSo << "/" << mauSo;
+    }
+
+    void rutGon() {
+        int ucln = timUCLN(tuSo, mauSo);
+        tuSo = tuSo / ucln;
+        mauSo = mauSo / ucln;
+        if (mauSo < 0) { tuSo = -tuSo; mauSo = -mauSo; }
+    }
+
+    // Cau 2: Phuong thuc Cong, Tru, Nhan, Chia
+    PhanSo cong(PhanSo ps2) {
+        PhanSo kq;
+        kq.tuSo = tuSo * ps2.mauSo + mauSo * ps2.tuSo;
+        kq.mauSo = mauSo * ps2.mauSo;
+        kq.rutGon();
+        return kq;
+    }
+
+    PhanSo tru(PhanSo ps2) {
+        PhanSo kq;
+        kq.tuSo = tuSo * ps2.mauSo - mauSo * ps2.tuSo;
+        kq.mauSo = mauSo * ps2.mauSo;
+        kq.rutGon();
+        return kq;
+    }
+
+    PhanSo nhan(PhanSo ps2) {
+        PhanSo kq;
+        kq.tuSo = tuSo * ps2.tuSo;
+        kq.mauSo = mauSo * ps2.mauSo;
+        kq.rutGon();
+        return kq;
+    }
+
+    PhanSo chia(PhanSo ps2) {
+        PhanSo kq;
+        kq.tuSo = tuSo * ps2.mauSo;
+        kq.mauSo = mauSo * ps2.tuSo;
+        kq.rutGon();
+        return kq;
     }
 };
 
